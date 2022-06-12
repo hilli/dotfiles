0cb486af42f97fd4b8182eddd5a1c0cf612b1c76
Jens Hilligsøe <github@hilli.dk>
2022-06-12 17:35:38 +0200

    Install some grpc tools

--- .chezmoiscripts/run_onchange_go_tools.sh
+++ .chezmoiscripts/run_onchange_go_tools.sh
@@ -2,4 +2,8 @@
 if [ "$(command -v go)" ]; then
   echo Installing go tools
   go install golang.org/x/review/git-codereview@latest
+  go install github.com/bufbuild/buf/cmd/buf@latest
+  go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+  go install github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go@latest
 fi
